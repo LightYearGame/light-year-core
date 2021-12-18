037637fd6ce44a6fe8ef0216215f45ced28f3c44
lch-howger <light_year_dev@163.com>
2021-12-18 21:35:01 +0800

    hero config

--- contracts/config/HeroConfig.sol
+++ contracts/config/HeroConfig.sol
@@ -11,11 +11,53 @@ contract HeroConfig is IHeroConfig {
         registryAddress = registry_;
     }
 
-    function getHeroPrice() public pure override returns (uint256){
-        return 0.06 ether;
+    mapping(uint256 => uint256) public override configs;
+
+    function setConfig(uint256 key_, uint256 value_) public returns (uint256){
+        configs[key_] = value_;
     }
 
-    function getAttribute(uint256) public pure override returns (uint256){
-        return 200;
+    function getHeroPrice(bool advance_) public pure override returns (uint256){
+        if (advance_) {
+            return 20;
+        } else {
+            return 10;
+        }
+    }
+
+    function randomHeroType(bool advance_, uint256 random_) public view override returns (uint256){
+        uint256 r1 = random_ % 100;
+        uint256 r2 = _random(random_, 12);
+        if (!advance_) {
+            if (r1 < 90) {
+                return r2;
+            } else if (r1 < 98) {
+                return r2 + 12;
+            } else {
+                return r2 + 24;
+            }
+        } else {
+            if (r1 < 80) {
+                return r2 + 12;
+            } else if (r1 < 98) {
+                return r2 + 24;
+            } else {
+                return r2 + 36;
+            }
+        }
+    }
+
+    /**
+     * random
+     */
+    function _random(uint256 seed_, uint256 randomSize_) private view returns (uint256){
+        uint256 nonce = seed_;
+        uint256 difficulty = block.difficulty;
+        uint256 gaslimit = block.gaslimit;
+        uint256 number = block.number;
+        uint256 timestamp = block.timestamp;
+        uint256 gasprice = tx.gasprice;
+        uint256 random = uint256(keccak256(abi.encodePacked(nonce, difficulty, gaslimit, number, timestamp, gasprice))) % randomSize_;
+        return random;
     }
 }
--- contracts/interface/IHeroConfig.sol
+++ contracts/interface/IHeroConfig.sol
@@ -3,7 +3,9 @@ pragma solidity ^0.6.12;
 
 interface IHeroConfig {
 
-    function getHeroPrice() external pure returns (uint256);
+    function getHeroPrice(bool advance_) external pure returns (uint256);
 
-    function getAttribute(uint256 shipId_) external pure returns (uint256);
+    function configs(uint256 key_) external view returns (uint256);
+
+    function randomHeroType(bool advance_, uint256 random_) external view returns (uint256);
 }
