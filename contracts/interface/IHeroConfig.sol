0f3b4ac36c889f167603760d2102f552d44376af
lch-howger <light_year_dev@163.com>
2022-01-04 14:41:01 +0800

    hero config

--- contracts/interface/IHeroConfig.sol
+++ contracts/interface/IHeroConfig.sol
@@ -1,5 +1,8 @@
 // SPDX-License-Identifier: MIT
+pragma experimental ABIEncoderV2;
 pragma solidity ^0.6.12;
+
+import "./IHero.sol";
 
 interface IHeroConfig {
 
@@ -8,4 +11,8 @@ interface IHeroConfig {
     function configs(uint256 key_) external view returns (uint256);
 
     function randomHeroType(bool advance_, uint256 random_) external view returns (uint256);
+
+    function getAttributesById(uint256 heroId_) external view returns (uint256[] memory);
+
+    function getAttributesByInfo(IHero.Info memory info_) external view returns (uint256[] memory);
 }
